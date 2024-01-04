2b3409f7dea25914b0f59d4edcde8044f51e46c0
Anderson Vera <70354869+JhonveraDev@users.noreply.github.com>
2024-01-03 19:46:55 -0500

    some modifications en recent users component

--- src/app/home/components/recent-users/recent-users.component.ts
+++ src/app/home/components/recent-users/recent-users.component.ts
@@ -11,7 +11,7 @@ export class RecentUsersComponent {
     { name: 'Gallante J.', items: 340, elements: 11.3, views: 4.1, avatar: '../../../../assets/avatar_30.png' },
     { name: 'Debit A.', items: 760, elements: 4.1, views: 12.3, avatar: '../../../../assets/avatar_31.png' },
     { name: 'Johnny E.', items: 760, elements: 1.3, views: 8.1, avatar: '../../../../assets/avatar_32.png' },
-    { name: 'Mark J.', items: 40, elements: 11, views: 4, avatar: '../../../../assets/avatar_33.png' },
+    { name: 'Mark J.', items: 40, elements: 11.5, views: 4, avatar: '../../../../assets/avatar_33.png' },
     { name: 'Hostetter R.', items: 885, elements: 15.4, views: 6.6, avatar: '../../../../assets/avatar_34.png' },
     { name: 'John P.', items: 769, elements: 8.3, views: 8.9, avatar: '../../../../assets/avatar_35.png' },
     { name: 'Austin R.', items: 842, elements: 15.6, views: 12.9, avatar: '../../../../assets/avatar_36.png' },
--- src/app/home/services/gallery-new-nfts.service.ts
+++ src/app/home/services/gallery-new-nfts.service.ts
@@ -8,14 +8,14 @@ export class GalleryNewNftsService {
   private images: NewNft[] = [
     { id: 1, title: 'Art work #2134', category: 'Fashion', url: '../../../../assets/1.jpeg', price: '0.29 ETH 7/21', user: 'Johny E.', avatar: '../../../../assets/avatar_13.png' },
     { id: 2, title: 'Cool octopus traveling', category: 'Fashion', url: '../../../../assets/2.jpeg', price: '0.24 ETH 4/17', user: 'Debit A.', avatar: '../../../../assets/avatar_12.png' },
-    { id: 3, title: 'Octopus eating icecrem', category: 'Music', url: '../../../../assets/3.jpeg', price: '0.09 ETH 1/9', user: 'Jhon V.', avatar: '../../../../assets/avatar_13.png' },
+    { id: 3, title: 'Octopus eating icecrem', category: 'Music', url: '../../../../assets/3.jpeg', price: '0.09 ETH 1/9', user: 'Jhon V.', avatar: '../../../../assets/avatar_36.png' },
     { id: 4, title: 'Panda with fish', category: 'Video', url: '../../../../assets/4.jpeg', price: '0.19 ETH 5/11', user: 'Jean P.', avatar: '../../../../assets/avatar_12.png' },
-    { id: 5, title: 'Kawaii-bubble-tea', category: 'Games', url: '../../../../assets/5.jpeg', price: '0.29 ETH 11/19', user: 'Austin R.', avatar: '../../../../assets/avatar_13.png' },
+    { id: 5, title: 'Kawaii-bubble-tea', category: 'Games', url: '../../../../assets/5.jpeg', price: '0.29 ETH 11/19', user: 'Austin R.', avatar: '../../../../assets/avatar_37.png' },
     { id: 6, title: 'Cute monsters hallo', category: 'Games', url: '../../../../assets/6.jpeg', price: '0.19 ETH 7/13', user: 'Saruman T.', avatar: '../../../../assets/avatar_12.png' },
-    { id: 7, title: 'Cat-mascot-character', category: 'Video', url: '../../../../assets/7.jpeg', price: '0.28 ETH 21/91', user: 'Gallant Z.', avatar: '../../../../assets/avatar_13.png' },
-    { id: 8, title: 'Small kid with orange', category: 'Games', url: '../../../../assets/8.jpeg', price: '0.17 ETH 14/23', user: 'Thorfin T.', avatar: '../../../../assets/avatar_12.png' },
+    { id: 7, title: 'Cat-mascot-character', category: 'Video', url: '../../../../assets/7.jpeg', price: '0.28 ETH 21/91', user: 'Gallant Z.', avatar: '../../../../assets/avatar_35.png' },
+    { id: 8, title: 'Small kid with orange', category: 'Games', url: '../../../../assets/8.jpeg', price: '0.17 ETH 14/23', user: 'Thorfin T.', avatar: '../../../../assets/avatar_33.png' },
     { id: 9, title: 'Digital Cat Illustration', category: 'Fashion', url: '../../../../assets/9.jpeg', price: '0.09 ETH 10/91', user: 'Roberto A.', avatar: '../../../../assets/avatar_13.png' },
-    { id: 10, title: 'Fictional character', category: 'Music', url: '../../../../assets/10.jpeg', price: '0.06 ETH 11/109', user: 'Samantha S.', avatar: '../../../../assets/avatar_12.png' },
+    { id: 10, title: 'Fictional character', category: 'Music', url: '../../../../assets/10.jpeg', price: '0.06 ETH 11/109', user: 'Samantha S.', avatar: '../../../../assets/avatar_32.png' },
   ];
 
   constructor() { }
